dd9c8248c29f8032510bb3b808eee0ed88bac357
ewhitebloom <ewhitebloom@gmail.com>
2014-03-28 12:01:43 -0400

    unit and acceptance working

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ActiveRecord::Base
+  validates :email, presence: true
+  validates :subject, presence: true
+  validates :description, presence: true
+  validates :first, presence: true
+  validates :last, presence: true
 end
---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe Contact do
+  describe "validations" do
+    let(:valid_attributes) do
+      {
+        email: "launch@academy.com",
+        subject: "launch academy",
+        description: "launchhhh",
+        first: "launch",
+        last: "academy"
+      }
+    end
+
+    let(:contact) { Contact.new(valid_attributes) }
+
+    it "is valid when it has all required and valid attributes" do
+      expect(contact).to be_valid
+    end
+
+    it "provides a count of spottings for a given bird" do
+      Contact.create!(email: "launch@academy.com", subject: "launch academy", description: "launchhhh", first: "launch", last: "academy")
+      Contact.create!(email: "launch@academy.com", subject: "launch academy", description: "launchhhh", first: "launch", last: "academy")
+      Contact.create!(email: "launch@academy.com", subject: "launch academy", description: "launchhhh", first: "launch", last: "academy")
+
+      expect(Contact.count).to eql(3)
+    end
+
+    it "requires an email" do
+      contact.email = ""
+      expect(contact).not_to be_valid
+    end
+
+     it "requires an subject" do
+      contact.subject = ""
+      expect(contact).not_to be_valid
+    end
+     it "requires an description" do
+      contact.description = ""
+      expect(contact).not_to be_valid
+    end
+     it "requires an first" do
+      contact.first = ""
+      expect(contact).not_to be_valid
+    end
+     it "requires an last" do
+      contact.last = ""
+      expect(contact).not_to be_valid
+    end
+
+  end
+end
