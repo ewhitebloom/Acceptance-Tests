8bcfc749585728ebdd91792b4fbd032f8a8f4ed6
ewhitebloom <ewhitebloom@gmail.com>
2014-03-28 11:39:56 -0400

    /

---
+++ spec/features/user_browses_contact_inquiries_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature "User visits the contact inquiry browser page" do
+# As an admin
+# I want to review contact inquiries
+# So that I can respond or take action
+
+it "sees a title" do
+  visit '/contacts'
+  expect(page).to have_content "Listing contacts"
+end
+
+describe "Creating Contacts" do
+  it "displays the contact text after creation" do
+    visit "/events"
+    click_link "New Contact"
+    email = "launch@academy.com"
+    fill_in "Email", with: email
+    fill_in "Subject", with: "Hi"
+    fill_in "Description", with: "Just saying hi."
+    fill_in "First", with: "Launch"
+    fill_in "Last", with: "Academy"
+    click_button "Create Contact"
+    expect(Contact.count).to eq(1)
+    expect(Contact.last.email).to eq(email)
+  end
+end
+
+it "sees contacts" do
+ visit '/contacts'
+ expect(page).to have_content()
+end
+
+end
